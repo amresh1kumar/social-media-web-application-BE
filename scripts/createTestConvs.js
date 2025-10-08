bf0e80503819935d8f527a00c2aba9b7a7d1c03c
amresh1kumar <amreshbkp0111@gmail.com>
2025-10-09 01:24:18 +0530

    update

--- scripts/createTestConvs.js
+++ scripts/createTestConvs.js
@@ -1,29 +1,3 @@
-// const mongoose = require("mongoose");
-// const Conversation = require("../src/models/Conversation");
-// const User = require("../src/models/User");
-// require("dotenv").config();
-
-// async function createTest() {
-//    await mongoose.connect(process.env.MONGO_URI);
-
-//    const users = await User.find().limit(2);
-//    if (users.length < 2) {
-//       console.log("Not enough users to create conversation");
-//       process.exit(0);
-//    }
-
-//    const conv = await Conversation.create({
-//       participants: [users[0]._id, users[1]._id],
-//       messages: [
-//          { sender: users[0]._id, text: "Hello!" },
-//          { sender: users[1]._id, text: "Hi, how are you?" }
-//       ]
-//    });
-
-//    console.log("Test conversation created:", conv);
-//    mongoose.disconnect();
-// }
-
 require('dotenv').config({ path: '../.env' });
 const mongoose = require('mongoose');
 const User = require('../src/models/User');
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/User");
-
-// module.exports = async function (req, res, next) {
-
-
-//    console.log("JWT_SECRET:", process.env.JWT_SECRET);
-//    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//    const token = req.header("Authorization")?.split(" ")[1];
-//    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
-
-//    try {
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       req.user = await User.findById(decoded.id).select("-password");
-//       if (!req.user) return res.status(401).json({ message: "User not found" });
-//       next();
-//    } catch (err) {
-//       console.error(err);
-//       res.status(401).json({ message: "Token is not valid" });
-//    }
-// };
-
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
